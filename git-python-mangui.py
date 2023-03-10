10aedf5ef24e29581c06abab1d378c02e75ab1db
ManguiEliezer <eliezermangui22@gmail.com>
2023-03-10 14:12:54 +0800

    My Education Background added

--- git-python-mangui.py
+++ git-python-mangui.py
@@ -4,3 +4,10 @@ loc = "Candahon, Mogpog, Marinduque"
 loc1 = str(input('Address: ' + loc))
 bday = "January 22, 2002"
 bday1 = str(input('Birthdate: ' + bday))
+print("\n")
+schl = 'College of Information and Conputer Sciences'
+schl1 = str(input("Colleger: " + schl))
+crs = 'Bachelor of Science in Information Technology'
+crs1 = str(input('Course: ' + crs))
+yr_sec = 'BSI/T 2B'
+yr_sec1 = str(input('Year and Section: ' + yr_sec))
