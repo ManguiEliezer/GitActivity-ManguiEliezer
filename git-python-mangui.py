24f55882a91cc877caf25ac4bf0aa308ed034b4a
ManguiEliezer <eliezermangui22@gmail.com>
2023-03-10 14:28:38 +0800

    My Hobbies added

--- git-python-mangui.py
+++ git-python-mangui.py
@@ -11,3 +11,11 @@ crs = 'Bachelor of Science in Information Technology'
 crs1 = str(input('Course: ' + crs))
 yr_sec = 'BSI/T 2B'
 yr_sec1 = str(input('Year and Section: ' + yr_sec))
+print('\n')
+str1 = 'Playing Badminton'
+str2 = 'Playing Musical Instruments'
+str3 = 'Singing'
+str4 = 'Photography'
+str5 = 'Film Making'
+print (input(str1),'\n' + input(str2),'\n' + input(str3),'\n' + input(str4),'\n' + input(str5),'\n')
+
