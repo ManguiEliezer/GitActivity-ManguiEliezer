0583a4d898cf9e604dc4793349b4690948b1b8d9
ManguiEliezer <eliezermangui22@gmail.com>
2023-03-10 10:10:41 +0800

    My Personal Info added

---
+++ git-python-mangui.py
@@ -0,0 +1,6 @@
+name = "Mangui, Eliezer L."
+name1 = str(input('Name: ' + name))
+loc = "Candahon, Mogpog, Marinduque"
+loc1 = str(input('Address: ' + loc))
+bday = "January 22, 2002"
+bday1 = str(input('Birthdate: ' + bday))
